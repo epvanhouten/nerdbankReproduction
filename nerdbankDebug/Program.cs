1bc6de56915bd868503e91274d471bd249f08dd0
Erick Van Houten <epvanhouten@gmail.com>
2020-03-14 08:10:02 -0500

    Setting up cs project

---
+++ nerdbankDebug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nerdbankDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
